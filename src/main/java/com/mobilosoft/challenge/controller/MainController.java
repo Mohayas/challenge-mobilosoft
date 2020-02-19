06a9f202aef1c57045a7be7a14b95a184e3f8f60
mohayas <mohayas@mohayas>
2020-02-19 23:03:49 +0000

    init project architecture

---
+++ src/main/java/com/mobilosoft/challenge/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.mobilosoft.challenge.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	// inject via application.properties
+	@Value("${welcome.message}")
+	private String message;
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("message", message);
+
+		return "index"; // view
+	}
+
+}
